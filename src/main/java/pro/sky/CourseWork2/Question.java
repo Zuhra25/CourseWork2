cd37413a0839d0883c4fd23451448283356980fa
ZU <zshaihova@gmail.com>
2023-09-12 20:43:46 +0300

    начальный коммит

---
+++ src/main/java/pro/sky/CourseWork2/Question.java
@@ -0,0 +1,12 @@
+package pro.sky.CourseWork2;
+
+public class Question {
+    String question;
+    String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+}
